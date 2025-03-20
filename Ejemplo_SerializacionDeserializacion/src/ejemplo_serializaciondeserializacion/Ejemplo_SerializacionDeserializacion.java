e9657b52148087cc5d9f314a2babdde00df60873
Zenaida Chacon <3016759880101@ingenieria.usac.edu.gt>
2025-03-19 18:11:27 -0600

    Ejemplo de Manejo de archivos clase 08

--- Ejemplo_SerializacionDeserializacion/src/ejemplo_serializaciondeserializacion/Ejemplo_SerializacionDeserializacion.java
+++ Ejemplo_SerializacionDeserializacion/src/ejemplo_serializaciondeserializacion/Ejemplo_SerializacionDeserializacion.java
@@ -3,7 +3,11 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package ejemplo_serializaciondeserializacion;
-
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
 /**
  *
  * @author zenai
@@ -15,6 +19,28 @@ public class Ejemplo_SerializacionDeserializacion {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Prueba de serializacion");
+        Persona persona = new Persona("Alejandro", 26);
+        try{
+            FileOutputStream archivo = new FileOutputStream("persona.ser");
+            ObjectOutputStream salida = new ObjectOutputStream(archivo);
+            salida.writeObject(persona);
+            System.out.println("Objeto serializado Correctamente");
+        }catch (IOException e){
+            System.out.println("el error es: "+ e.toString());
+        }
+        try{
+            FileInputStream archivoD = new FileInputStream ("persona.ser");
+            ObjectInputStream entrada = new ObjectInputStream(archivoD);
+            Persona per = (Persona) entrada.readObject();
+            System.out.println("Nombre: "+per.getNombre());
+            System.out.println("Edad: "+per.getEdad());
+            
+            
+        }catch (IOException|ClassNotFoundException e){
+            e.printStackTrace();
+        }
+        
     }
     
 }
---
+++ Ejemplo_SerializacionDeserializacion/src/ejemplo_serializaciondeserializacion/Persona.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejemplo_serializaciondeserializacion;
+import java.io.Serializable;
+/**
+ *
+ * @author zenai
+ */
+public class Persona implements Serializable{
+    private static final long serialVersionUID = 1L;
+    private String nombre;
+    private int edad;
+
+    public Persona(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    
+    
+    
+}
